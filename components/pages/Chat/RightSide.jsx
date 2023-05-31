e17e54f9b3421e9cc0fef969817ce4afd8d3602b
shahbaz <shahbaz.pro2@gmail.com>
2023-06-01 01:37:54 +0500

    Change date formate

--- components/pages/Chat/RightSide.jsx
+++ components/pages/Chat/RightSide.jsx
@@ -93,8 +93,15 @@ const RightSide = ({ filteredMessages }) => {
                                 <div className='flex justify-center items-center'>
                                     <div className='text-gray-500 text-sm'>
                                         {
-                                            moment(date, 'DD/MM/YYYY').isSame(moment(), 'day') ? 'Today' : moment(date, 'DD/MM/YYYY').isSame(moment().subtract(1, 'days'), 'day') ? 'Yesterday' : moment(date, 'DD/MM/YYYY').isSame(moment(), 'week') ? moment(date, 'DD/MM/YYYY').format('dddd') : date
+                                            moment(date, 'DD/MM/YYYY').format('DD/MM/YYYY') === moment().format('DD/MM/YYYY') ? 'Today' : moment(date, 'DD/MM/YYYY').format('DD/MM/YYYY') === moment().subtract(1, 'days').format('DD/MM/YYYY') ? 'Yesterday' : moment(date, 'DD/MM/YYYY').isSame(moment(), 'week') ? moment(date, 'DD/MM/YYYY').format('dddd') : moment(date, 'DD/MM/YYYY').format('YYYY') === moment().format('YYYY') ? moment(date, 'DD/MM/YYYY').format('ddd, MMM YY') : moment(date, 'DD/MM/YYYY').format('MMM DD, YYYY')
+
+
+
+
+
                                         }
+
+
                                     </div>
                                 </div>
                                 <div className="space-y-2" >
