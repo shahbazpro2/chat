9e511f4569814ef2e0eaa01a1497e3a866eed27d
Muhammad <shahbaz.pro2@gmail.com>
2025-02-24 02:29:12 +0500

    Fix issues

--- components/pages/users/users.js
+++ components/pages/users/users.js
@@ -1,6 +1,7 @@
 import { faker } from "@faker-js/faker";
+const ids = [3, 5, 6, 7];
 export const users = Array.from({ length: 4 }, (_, index) => ({
-  id: index + 3,
+  id: ids[index],
   name: faker.person.fullName(),
   email: faker.internet.email(),
   jobType: faker.name.jobType(),
--- src/pages/api/messages/index.js
+++ src/pages/api/messages/index.js
@@ -1,120 +1,112 @@
-import { upload } from '@backend/utils/awsS3';
-import { PrismaClient } from '@prisma/client'
-import formidable from 'formidable';
-const prisma = new PrismaClient()
+import { upload } from "@backend/utils/awsS3";
+import { PrismaClient } from "@prisma/client";
+import formidable from "formidable";
+const prisma = new PrismaClient();
 
 export const config = {
-    api: {
-        bodyParser: false,
-
-    },
+  api: {
+    bodyParser: false,
+  },
 };
 
 const createMessage = async (req, res, newChatId) => {
-    return new Promise((resolve, reject) => {
-        upload.single('file')(req, res, async (err) => {
-            const { text, senderId, receiverId, chatId } = req.body || {};
-            if (!text && !req?.file?.location) {
-                reject('No text or file provided')
-                return
-            }
-            if (err) {
-                //failed to upload file
-                return reject(err);
-            }
+  return new Promise((resolve, reject) => {
+    upload.single("file")(req, res, async (err) => {
+      const { text, senderId, receiverId, chatId } = req.body || {};
+      if (!text && !req?.file) {
+        reject("No text or file provided");
+        return;
+      }
+      if (err) {
+        //failed to upload file
+        return reject(err);
+      }
 
-            if (Number(chatId)) {
-                const createRes = await prisma.message.create({
-                    data: {
-                        text: text || '',
-                        file: req?.file?.location || null,
-                        senderId: Number(senderId),
-                        chatId: Number(chatId)
-                    }
-                })
-                //only change update at
-                await prisma.chat.update({
-                    where: {
-                        id: Number(chatId)
-                    },
-                    data: {
-                        updatedAt: new Date()
-                    }
-                })
-                resolve(createRes);
-
-            } else {
-                //create a new chat
-                const newChat = await prisma.chat.create({
-                    data: {
-                        members: {
-                            connect: [
-                                { id: Number(senderId) },
-                                { id: Number(receiverId) }
-                            ]
-                        }
-                    },
-                    include: {
-                        members: true
-                    }
-                });
-                await prisma.message.create({
-                    data: {
-                        text: text || '',
-                        file: req?.file?.location || null,
-                        senderId: Number(senderId),
-                        chatId: newChat.id
-                    }
-                })
-                resolve(newChat);
-            }
-
+      if (Number(chatId)) {
+        const createRes = await prisma.message.create({
+          data: {
+            text: text || "",
+            file: req?.file?.location || null,
+            senderId: Number(senderId),
+            chatId: Number(chatId),
+          },
         });
-    })
-
-}
+        //only change update at
+        await prisma.chat.update({
+          where: {
+            id: Number(chatId),
+          },
+          data: {
+            updatedAt: new Date(),
+          },
+        });
+        resolve(createRes);
+      } else {
+        //create a new chat
+        const newChat = await prisma.chat.create({
+          data: {
+            members: {
+              connect: [{ id: Number(senderId) }, { id: Number(receiverId) }],
+            },
+          },
+          include: {
+            members: true,
+          },
+        });
+        await prisma.message.create({
+          data: {
+            text: text || "",
+            file: req?.file?.location || null,
+            senderId: Number(senderId),
+            chatId: newChat.id,
+          },
+        });
+        resolve(newChat);
+      }
+    });
+  });
+};
 
 export default async function handler(req, res) {
-    switch (req.method) {
-        case 'POST':
-            try {
-                const message = await createMessage(req, res)
-                return res.status(200).json(message);
-            } catch (err) {
-                return res.status(500).json({ error: err })
-            }
-        case 'GET':
-            const { ids } = req.query;
-            console.log('iddd', ids)
-            //split the ids into int and into an array
-            const chatIds = ids?.split(',').map(id => parseInt(id));
-            //get all measseges according to single chats and sperate them according to the single chat id while querying
-            const singleChat = await prisma.message.findMany({
+  switch (req.method) {
+    case "POST":
+      try {
+        const message = await createMessage(req, res);
+        return res.status(200).json(message);
+      } catch (err) {
+        return res.status(500).json({ error: err });
+      }
+    case "GET":
+      const { ids } = req.query;
+      console.log("iddd", ids);
+      //split the ids into int and into an array
+      const chatIds = ids?.split(",").map((id) => parseInt(id));
+      //get all measseges according to single chats and sperate them according to the single chat id while querying
+      const singleChat = await prisma.message.findMany({
+        where: {
+          chatId: {
+            in: chatIds,
+          },
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+        include: {
+          sender: true,
+        },
+      });
+      //seprate all the messages accourding to the single chat id
+      const chatMessages = singleChat.reduce((acc, message) => {
+        const { chatId } = message;
+        if (!acc[chatId]) {
+          acc[chatId] = [];
+        }
+        acc[chatId].push(message);
+        return acc;
+      }, {});
 
-                where: {
-                    chatId: {
-                        in: chatIds
-                    }
-                },
-                orderBy: {
-                    createdAt: 'asc'
-                },
-                include: {
-                    sender: true
-                }
-            })
-            //seprate all the messages accourding to the single chat id
-            const chatMessages = singleChat.reduce((acc, message) => {
-                const { chatId } = message;
-                if (!acc[chatId]) {
-                    acc[chatId] = [];
-                }
-                acc[chatId].push(message);
-                return acc;
-            }, {});
-
-            return res.status(200).json(chatMessages);
-        default:
-            return res.status(405).json({ message: 'Method not allowed' });
-    }
+      return res.status(200).json(chatMessages);
+    default:
+      return res.status(405).json({ message: "Method not allowed" });
+  }
 }
